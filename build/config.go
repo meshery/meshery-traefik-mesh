b2b0d0b0ceef36b3dcb2776ccb7f62f0a9b2fee5
ashish <ashishjaitiwari15112000@gmail.com>
2022-01-25 18:23:26 +0530

    Fix #159
    Signed-off-by: ashish <ashishjaitiwari15112000@gmail.com>

--- build/config.go
+++ build/config.go
@@ -1,22 +1,23 @@
 package build
 
 import (
-	"io/ioutil"
-	"net/http"
+	"fmt"
 	"os"
 	"path/filepath"
 
 	"github.com/layer5io/meshery-adapter-library/adapter"
-	"github.com/layer5io/meshery-traefik-mesh/internal/config"
+	"github.com/layer5io/meshkit/utils"
 	"github.com/layer5io/meshkit/utils/manifests"
+	walker "github.com/layer5io/meshkit/utils/walker"
 	smp "github.com/layer5io/service-mesh-performance/spec"
-	"gopkg.in/yaml.v2"
 )
 
 var DefaultVersion string
 var DefaultURL string
-var DefaultGenerationMethod = adapter.HelmCHARTS
+var DefaultGenerationMethod string
 var WorkloadPath string
+var AllVersions []string
+var CRDNames []string
 
 //NewConfig creates the configuration for creating components
 func NewConfig(version string) manifests.Config {
@@ -37,40 +38,30 @@ func NewConfig(version string) manifests.Config {
 	}
 }
 
-// returns latest valid appversion and chartversion
-func getLatestValidAppVersionAndChartVersion() (string, string, error) {
-	res, err := http.Get("https://helm.traefik.io/traefik/index.yaml")
-	if err != nil {
-		return "", "", config.ErrGetLatestReleases(err)
-	}
-	content, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return "", "", config.ErrGetLatestReleases(err)
-	}
-	var h helmIndex
-	err = yaml.Unmarshal(content, &h)
-	if err != nil {
-		return "", "", config.ErrGetLatestReleases(err)
-	}
-
-	return h.Entries["traefik"][0].AppVersion, h.Entries["traefik"][0].Version, nil
-}
-
-// Below structs are helper structs to unmarshall and extract certain fields from helm chart's index.yaml
-type helmIndex struct {
-	Entries map[string][]data `yaml:"entries"`
-}
-
-type data struct {
-	AppVersion string `yaml:"appVersion"`
-	Version    string `yaml:"version"`
-}
-
 func init() {
 	wd, _ := os.Getwd()
 	WorkloadPath = filepath.Join(wd, "templates", "oam", "workloads")
-	var chartv string
-	DefaultVersion, chartv, _ = getLatestValidAppVersionAndChartVersion()
-	DefaultURL = "https://helm.traefik.io/traefik/traefik-" + chartv + ".tgz"
+	AllVersions, _ = utils.GetLatestReleaseTagsSorted("traefik", "mesh")
+	if len(AllVersions) == 0 {
+		return
+	}
+	DefaultVersion = AllVersions[len(AllVersions)-1]
+	DefaultGenerationMethod = adapter.Manifests
 
+	//Get all the crd names
+	w := walker.NewGithub()
+	err := w.Owner("traefik").
+		Repo("mesh-helm-chart").
+		Branch("master").
+		Root("mesh/crds/**").
+		RegisterFileInterceptor(func(gca walker.GithubContentAPI) error {
+			if gca.Content != "" {
+				CRDNames = append(CRDNames, gca.Name)
+			}
+			return nil
+		}).Walk()
+	if err != nil {
+		fmt.Println("Could not find CRD names. Will fail component creation...", err.Error())
+	}
+	DefaultURL = "https://raw.githubusercontent.com/traefik/mesh-helm-chart/" + "master" + "/mesh/crds/"
 }
